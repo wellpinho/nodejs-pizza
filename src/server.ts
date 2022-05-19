4f82444fa7304c4e82afe11d33d80d39fb6173bc
wellpinho <wellpinho@outlook.com>
2022-05-19 19:15:05 -0300

    ✨ NEW FEATURE: errors and cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
 require('dotenv').config()
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
+// qualquer ip pode acessar o app
+app.use(cors());
+
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 const port = process.env.PORT || 4000
 
